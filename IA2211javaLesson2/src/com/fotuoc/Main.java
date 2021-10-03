b5ba935d7904fa39e9359da28d9b42e4bfd513f3
Roshan0531 <2019t00351@stu.cmb.ac.lk>
2021-10-04 00:38:26 +0530

    Added if examples to main

--- IA2211javaLesson2/src/com/fotuoc/Main.java
+++ IA2211javaLesson2/src/com/fotuoc/Main.java
@@ -2,7 +2,36 @@ package com.fotuoc;
 
 public class Main {
 
+    static String name = "Roshan";
+    public static final int serialNum = 10;
+    boolean isActive = false;
+
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Name: " + name);
+
+        String myName = "Roshan" + " " + "Thushara";
+        System.out.println(myName);
+
+        int valA = 10;
+        double valB = (double) valA;
+
+        //specific reason
+        int valC = valA / (int) 2.5;
+        System.out.println("valC = " + valC);
+
+        int age = 88;
+
+        if (age < 18) {
+
+            System.out.println("Not eligible for a driving license");
+        }
+        else if (age > 80) {
+            System.out.println("Too old to drive. please submit a health check results");
+        }
+        else {
+            System.out.println("Allowed to a apply");
+        }
     }
 }
